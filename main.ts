0e705684b62317dc7d400b715bbc6cad1107c84a
Subhan-Asghar <subhanasghar16@gmail.com>
2025-05-21 22:52:24 +0500

    google auth

--- main.ts
+++ main.ts
@@ -1,9 +1,22 @@
-import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { google } from "googleapis";
+import path from "path";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 
-const server=new McpServer({
-    name:"GDrive",
-    version:"1.0.0"
+const server = new McpServer({
+  name: "GDrive",
+  version: "1.0.0",
+});
 
-})
+const Google_auth = () => {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: path.join(__dirname, "auth-file.json"),
+    scopes: ["https://www.googleapis.com/auth/drive"],
+  });
+
+  return google.drive({ version: "v3", auth });
+  
+};
+
+
