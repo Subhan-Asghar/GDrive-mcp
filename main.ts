c2c4e0727d0b900eb5c970b8826ce7fa49c41a00
Subhan-Asghar <subhanasghar16@gmail.com>
2025-05-21 21:07:24 -0700

    Setup

---
+++ main.ts
@@ -0,0 +1,9 @@
+import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+const server=new McpServer({
+    name:"GDrive",
+    version:"1.0.0"
+
+})
